d2b1a4abf7eb9a2aa46d87c9601ef560c4964d76
somayehbd <somayehbd@gmail.com>
2023-01-31 22:59:58 +0100

    added different function declarations

---
+++ function-types/index.js
@@ -0,0 +1,20 @@
+// Normal Function
+function normalFunc() {
+    console.log('I am a normal function.');
+}
+
+normalFunc();
+
+// Arrow Function
+const arrowFunc = () => {
+    console.log('I am arrow function.');
+}
+
+arrowFunc();
+
+// Anonymous Function
+const anonymousFunc = function() {
+    console.log('I am anonymous function.');
+}
+
+anonymousFunc();
